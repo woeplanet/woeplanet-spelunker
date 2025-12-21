2483686214fdf420a7a694ed95833e99a169bed2
Gary Gale <gary@vicchi.org>
2025-12-21 16:53:29 +0000

    feat: add /about page handling

---
+++ src/woeplanet/spelunker/pages/about.py
@@ -0,0 +1,37 @@
+"""
+WOEplanet Spelunker: pages package; about page module
+"""
+
+import logging
+
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+
+from woeplanet.spelunker.config.place_scale import placetype_to_scale
+from woeplanet.spelunker.dependencies.templates import get_templater
+from woeplanet.spelunker.pages.random import _random_place
+
+logger = logging.getLogger(__name__)
+
+
+async def about_endpoint(request: Request) -> HTMLResponse:
+    """
+    About page endpoint
+    """
+
+    place = await _random_place(request=request)
+    template = get_templater().get_template('about.html.j2')
+    template_args = {
+        'map': True,
+        'centroid': place.get('centroid'),
+        'bounds': place.get('bounds'),
+        'title': 'About',
+        'woeid': place.get('woe_id'),
+        'iso': place.get('iso', 'GB'),
+        'nearby': place.get('woe_id'),
+        'name': place.get('name'),
+        'scale': placetype_to_scale(int(place.get('placetype_id', 0))),
+        'doc': place,
+    }
+    content = await template.render_async(request=request, **template_args)
+    return HTMLResponse(content)
